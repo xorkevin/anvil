5e39629ae65589d209fd6d9fac26e322944de4d4
Kevin Wang <kevin@xorkevin.com>
2021-05-02 16:44:06 -0700

    Impl gitSwitch

--- component/fetcher.go
+++ component/fetcher.go
@@ -11,19 +11,19 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+
+	"golang.org/x/mod/semver"
 )
 
 const (
 	binGit = "git"
 )
 
-const (
-	stderrMsgCap = 4096
-)
-
 var (
 	// ErrCacheNotGitRepo is returned when a cache file is not a git repo
 	ErrCacheNotGitRepo = errors.New("Cache dir is not git repo")
+	// ErrGitBinVersion is returned when a git binary is not the correct version
+	ErrGitBinVersion = errors.New("Invalid git version")
 )
 
 type (
@@ -66,20 +66,41 @@ func (o *OSFetcher) FetchGit(ctx context.Context, repo, ref string) error {
 	if err != nil && !errors.Is(err, fs.ErrNotExist) {
 		return fmt.Errorf("Failed to check if %s already cloned: %w", repo, err)
 	}
+	alreadyCloned := err == nil
 	if !info.IsDir() {
 		return fmt.Errorf("%w: %s", ErrCacheNotGitRepo, repopath)
 	}
-	if err == nil {
-		if err := o.gitPull(ctx, repopath, repo); err != nil {
+	if err := o.gitVersion(ctx); err != nil {
+		return err
+	}
+	alreadySwitched := false
+	if alreadyCloned {
+		if err := o.gitPull(ctx, repopath, ref); err != nil {
 			return err
 		}
+		alreadySwitched = true
 	} else {
 		if err := o.gitClone(ctx, repopath, repo); err != nil {
 			return err
 		}
 	}
+	if o.gitIsBranch(ctx, repopath, ref) {
+		if !alreadySwitched {
+			if err := o.gitSwitchBranch(ctx, repopath, ref); err != nil {
+				return err
+			}
+		}
+	}
+	// is tag or commit
+	if err := o.gitSwitchDetach(ctx, repopath, ref); err != nil {
+		return err
+	}
 	return nil
 }
+
+const (
+	stderrMsgCap = 4096
+)
 
 func (o *OSFetcher) runCmd(cmd *exec.Cmd) error {
 	stderr, err := cmd.StderrPipe()
@@ -103,6 +124,28 @@ func (o *OSFetcher) runCmd(cmd *exec.Cmd) error {
 	return nil
 }
 
+const (
+	gitMinVersion    = "v2.23"
+	gitVersionPrefix = "git version "
+)
+
+func (o *OSFetcher) gitVersion(ctx context.Context) error {
+	b := &bytes.Buffer{}
+	cmd := exec.CommandContext(ctx, binGit, "--version")
+	cmd.Stdout = b
+	if err := o.runCmd(cmd); err != nil {
+		return fmt.Errorf("Failed to get git version: %w", err)
+	}
+	out := strings.TrimSpace(b.String())
+	if !strings.HasPrefix(out, gitVersionPrefix) {
+		return fmt.Errorf("%w: %s", ErrGitBinVersion, out)
+	}
+	if semver.Compare(gitMinVersion, fmt.Sprintf("v%s", strings.TrimPrefix(out, gitVersionPrefix))) > 0 {
+		return fmt.Errorf("%w: %s", ErrGitBinVersion, out)
+	}
+	return nil
+}
+
 func (o *OSFetcher) gitClone(ctx context.Context, repopath, repo string) error {
 	args := make([]string, 0, 6)
 	args = append(args, "clone", "--origin", "origin")
@@ -116,31 +159,35 @@ func (o *OSFetcher) gitClone(ctx context.Context, repopath, repo string) error {
 	return nil
 }
 
-func (o *OSFetcher) gitPull(ctx context.Context, repopath, ref string) error {
-	branch := ref
-	if !o.gitIsBranch(ctx, repopath, ref) {
-		b := &bytes.Buffer{}
-		cmd := exec.CommandContext(ctx, binGit, "symbolic-ref", "refs/remotes/origin/HEAD")
-		cmd.Stdout = b
-		if err := cmd.Run(); err == nil {
-			out := strings.TrimSpace(b.String())
-			branch = strings.TrimPrefix(out, "refs/remotes/origin/")
-			if branch == out {
-				branch = "master"
-			}
-		} else {
-			branch = "master"
-		}
+func (o *OSFetcher) gitPull(ctx context.Context, repopath, branch string) error {
+	if !o.gitIsBranch(ctx, repopath, branch) {
+		branch = o.gitGetDefaultBranch(ctx, repopath)
 	}
+	if err := o.gitSwitchBranch(ctx, repopath, branch); err != nil {
+		return err
+	}
+	cmd := exec.CommandContext(ctx, binGit, "pull", "--ff-only")
+	cmd.Dir = repopath
+	if err := o.runCmd(cmd); err != nil {
+		return fmt.Errorf("Failed to git pull %s in %s: %w", branch, repopath, err)
+	}
+	return nil
+}
+
+func (o *OSFetcher) gitSwitchBranch(ctx context.Context, repopath, branch string) error {
 	cmd := exec.CommandContext(ctx, binGit, "switch", branch)
 	cmd.Dir = repopath
 	if err := o.runCmd(cmd); err != nil {
 		return fmt.Errorf("Failed to git switch to %s in %s: %w", branch, repopath, err)
 	}
-	cmd = exec.CommandContext(ctx, binGit, "pull", "--ff-only")
+	return nil
+}
+
+func (o *OSFetcher) gitSwitchDetach(ctx context.Context, repopath, ref string) error {
+	cmd := exec.CommandContext(ctx, binGit, "switch", "--detach", ref)
 	cmd.Dir = repopath
 	if err := o.runCmd(cmd); err != nil {
-		return fmt.Errorf("Failed to git pull %s in %s: %w", branch, repopath, err)
+		return fmt.Errorf("Failed to git switch to %s in %s: %w", ref, repopath, err)
 	}
 	return nil
 }
@@ -149,15 +196,46 @@ func (o *OSFetcher) gitIsBranch(ctx context.Context, repopath, ref string) bool 
 	if ref == "" {
 		return false
 	}
-	cmd := exec.CommandContext(ctx, binGit, "show-ref", "--verify", fmt.Sprintf("refs/heads/%s", ref))
+	cmd := exec.CommandContext(ctx, binGit, "show-ref", "--quiet", "--verify", fmt.Sprintf("refs/heads/%s", ref))
 	cmd.Dir = repopath
 	if err := cmd.Run(); err == nil {
 		return true
 	}
-	cmd = exec.CommandContext(ctx, binGit, "show-ref", "--verify", fmt.Sprintf("refs/remotes/origin/%s", ref))
+	cmd = exec.CommandContext(ctx, binGit, "show-ref", "--quiet", "--verify", fmt.Sprintf("refs/remotes/origin/%s", ref))
 	cmd.Dir = repopath
 	if err := cmd.Run(); err == nil {
 		return true
 	}
 	return false
+}
+
+func (o *OSFetcher) gitIsTag(ctx context.Context, repopath, ref string) bool {
+	if ref == "" {
+		return false
+	}
+	cmd := exec.CommandContext(ctx, binGit, "show-ref", "--quiet", "--verify", fmt.Sprintf("refs/tags/%s", ref))
+	cmd.Dir = repopath
+	if err := cmd.Run(); err == nil {
+		return true
+	}
+	return false
+}
+
+const (
+	gitDefaultBranch   = "master"
+	gitRefOriginPrefix = "refs/remotes/origin/"
+)
+
+func (o *OSFetcher) gitGetDefaultBranch(ctx context.Context, repopath string) string {
+	b := &bytes.Buffer{}
+	cmd := exec.CommandContext(ctx, binGit, "symbolic-ref", "refs/remotes/origin/HEAD")
+	cmd.Stdout = b
+	if err := cmd.Run(); err != nil {
+		return gitDefaultBranch
+	}
+	out := strings.TrimSpace(b.String())
+	if !strings.HasPrefix(out, gitRefOriginPrefix) {
+		return gitDefaultBranch
+	}
+	return strings.TrimPrefix(out, gitRefOriginPrefix)
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/mod v0.4.2
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
